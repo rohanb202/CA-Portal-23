f30cdcfa7bfeefba2a6683c4ae6039d2048e8f3f
Samar Singh Randhawa <98534735+Samar1110@users.noreply.github.com>
2022-10-02 19:34:01 +0530

    Add files via upload

---
+++ src/Contact/src/Buttons.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './Buttons.css'
+
+export default function Buttons () {
+    function ripple(e){
+        // console.log(e);
+        const x=e.pageX-e.target.offsetLeft;
+        const y=e.pageY-e.target.offsetTop;
+        e.target.style.setProperty('--x',x+'px')
+        e.target.style.setProperty('--y',y+'px')
+
+    }
+    return (
+        <div className='buttons'>
+            <p className='btn' onMouseMove={ripple}><span className='gridContent'>Lorem Ipsum</span></p>
+            <p className='btn' onMouseMove={ripple}><span className='gridContent'>Lorem Ipsum</span></p>
+        </div>
+        
+    );
+};
---
+++ src/Contact/src/index.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import Buttons from './Buttons'
+// import './Buttons.css'
+import Footer from './Footer'
+// import './Footer.css'
+// import reportWebVitals from './reportWebVitals';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+    <Buttons />
+    <Footer />
+  </React.StrictMode>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// reportWebVitals();
