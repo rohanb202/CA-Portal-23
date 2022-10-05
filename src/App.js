a678e21c91b693f6e3d6a36f93fdfe754a085e88
MayankNagvanshi <87947863+MayankNagvanshi@users.noreply.github.com>
2022-10-06 00:58:23 +0530

    brownie_points

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import logo from './logo.svg';
 import './App.css';
+import Services from "./components/Services/Services";
+import { useContext } from "react";
+import { themeContext } from "./Context";
 import Header from './components/header/header';
 import Contact from './Contact/Contact';
 import aos from 'aos';
@@ -8,6 +11,8 @@ import { useEffect } from 'react';
 
 function App() {
 
+  // const theme = useContext(themeContext);
+  // const darkMode = theme.state.darkMode;
   // useEffect(() => {
   //   aos.init({duration: 3000});
   // }, []);
@@ -15,6 +20,10 @@ function App() {
   return (
     <>
       <Header />
+     
+        <Services />
+    
+
       <Contact />
     </>
   );
