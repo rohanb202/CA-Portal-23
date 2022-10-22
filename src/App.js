82f4f8c41bf96502227c1e973c0c1aad353da57d
rohanb202 <rohanb22250@gmail.com>
2022-10-22 22:10:56 +0530

    login-main

--- src/App.js
+++ src/App.js
@@ -1,46 +1,42 @@
-import './App.css';
+import "./App.css";
 // import Brownie from "./components/New_brownie/Brownie_point";
 // import Nduty from "./components/New_Duty/Nduty"
 
 // import Contact from './Contact/Contact';
-import Login1 from './components/Login1/Login1';
-import ForgotPass from './components/ForgotPass/forgotPass';
+import Login1 from "./components/Login1/Login1";
+import LoginMain from "./components/LoginMain/LoginMain";
+import ForgotPass from "./components/ForgotPass/forgotPass";
 // import Login from './components/Login/Login';
 // import Main from './components/main/main';
-
-
 
 import "aos/dist/aos.css";
 
 // import { useEffect, useState } from 'react';
-import Home from './components/Home';
-import {Routes,Route} from "react-router-dom";
-import LeaderBoard from './components/leaderboard/LeaderBoard';
-import Profile from './Pages/Profile/Profile';
-import AmbassadorApplyForm from './Pages/Ambassodor/Application/ApplicationForm';
+import Home from "./components/Home";
+import { Routes, Route } from "react-router-dom";
+import LeaderBoard from "./components/leaderboard/LeaderBoard";
+import Profile from "./Pages/Profile/Profile";
+import AmbassadorApplyForm from "./Pages/Ambassodor/Application/ApplicationForm";
 // import Login from './components/Login/Login';
-import LoginPage from './Pages/LoginPage';
+import LoginPage from "./Pages/LoginPage";
 // import { AuthProvider } from './context/AuthContext'
-import { ToastContainer, toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 function App() {
- 
   return (
     <>
-    <ToastContainer />
+      <ToastContainer />
       <Routes>
-        <Route path='/' element={<Home />}></Route> 
+        <Route path="/" element={<Home />}></Route>
         <Route path="/leaderboard" element={<LeaderBoard />}></Route>
-        <Route path='/ambassador-apply' element={<AmbassadorApplyForm />} />
-        <Route path='/profile' element={<Profile />} />
-        <Route path='/login1' element={<Login1 />} />
-        <Route path='/forgotpass' element={<ForgotPass />} />
+        <Route path="/ambassador-apply" element={<AmbassadorApplyForm />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/login1" element={<LoginMain />} />
+        <Route path="/forgotpass" element={<ForgotPass />} />
       </Routes>
     </>
-  )
-  
-
+  );
 }
 
 export default App;
---
+++ src/components/LoginMain/LoginMain.js
@@ -0,0 +1,63 @@
+import React from "react";
+import stripes from "./stripes.svg";
+import logo from "./logo.svg";
+import { useContext } from "react";
+import AuthContext from "../../context/AuthContext";
+import { Link } from "react-router-dom";
+function LoginMain() {
+  let { loginUser } = useContext(AuthContext);
+  return (
+    <div className="flex items-center justify-center h-screen">
+      <div className="w-[50vw] hidden md:block">
+        <img src={stripes} alt="" className="object-cover h-screen" />
+        {/* <div className="h-screen bgStripes"></div> */}
+      </div>
+
+      <form
+        onSubmit={loginUser}
+        class="flex flex-col space-y-5 justify-center items-center px-10"
+      >
+        <img src={logo} alt="" className="w-2/3 min-w-[18rem]" />
+
+        <label className="block w-6/12 min-w-[15rem]">
+          <span className="text-gray-700 whitespace-nowrap">Email ID</span>
+          <input
+            type="email"
+            className="block w-full mt-1 border-gray-300 rounded-md shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
+            placeholder=""
+          />
+        </label>
+        <label class="block  w-6/12 min-w-[15rem]">
+          <span class="text-gray-700">Password</span>
+          <input
+            type="password"
+            class="
+                    mt-1
+                    block
+                    w-full
+                    rounded-md
+                    border-gray-300
+                    shadow-sm
+                    focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50
+                  "
+          />
+        </label>
+        <Link to="/forgotpass" className="text-sm text-[#F74061]">
+          Forgot password?
+        </Link>
+        <button
+          type="submit"
+          className="bg-[#F74061] rounded-lg w-36 p-2 text-white font-semibold hover:scale-110 transition-all ease-in-out"
+        >
+          Login
+        </button>
+        <span class="text-sm text-[#F74061]">
+          <span className="text-[#F79840]">Don't have an account?</span>
+          <Link to="/login1"> Sign up</Link>
+        </span>
+      </form>
+    </div>
+  );
+}
+
+export default LoginMain;
