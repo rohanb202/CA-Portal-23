367fcd4b9c505523d60514c33a901cc5a01641af
Vysakh A V <vysakhav951@gmail.com>
2022-10-23 15:03:48 +0530

    fix login loader

--- src/components/LoginMain/LoginMain.js
+++ src/components/LoginMain/LoginMain.js
@@ -7,27 +7,27 @@ import { Link } from "react-router-dom";
 import LoaderCss from "./LoginMain.module.scss"
 function LoginMain() {
   
-  const [variable,setVariable]=useState(true);
+  const [requesting, setRequesting] = useState(false);
+  // const [email, setEmail] = useState("")
+  // const [password, setPassword] = useState("")
+  const [formData, setformData] = useState({email: "", password: ""})
   let { loginUser } = useContext(AuthContext);
+
+  const handleChange = (e) => {
+    setformData({...formData,[e.target.name]:e.target.value})
+  }
   
-  function handleClick(e) {
+  async function handleClick(e) {
     e.preventDefault();
-    setVariable(()=>{
-      return false;
-    })
-    
-    loginUser(e);
-    setVariable(()=>{
-      return true;
-    })
+
+    setRequesting(true);
+    await loginUser(formData.email, formData.password);
+    setRequesting(false);
 
   }
 
-  console.log(variable);
 
   return (
-
-    
 
     <div className="flex items-center justify-center h-screen">
       <div className="w-[50vw] hidden md:block">
@@ -35,25 +35,25 @@ function LoginMain() {
         {/* <div className="h-screen bgStripes"></div> */}
       </div>
 
-      <form
-        onSubmit={handleClick}
-        class="flex flex-col space-y-5 justify-center items-center px-10"
-      >
+      <form onSubmit={handleClick} className="flex flex-col space-y-5 justify-center items-center px-10">
         <img src={logo} alt="" className="w-2/3 min-w-[18rem]" />
 
         <label className="block w-6/12 min-w-[15rem]">
           <span className="text-gray-700 whitespace-nowrap">Email ID</span>
           <input
-            type="email"
-            className="block w-full mt-1 border-gray-300 rounded-md shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50" name="email"
-            placeholder=""
-          />
+            type="email" 
+            onChange={handleChange}
+            className="block w-full mt-1 border-gray-300 rounded-md shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50" 
+            name="email"
+            placeholder="" required={true}/>
         </label>
-        <label class="block  w-6/12 min-w-[15rem]">
-          <span class="text-gray-700">Password</span>
+        <label className="block  w-6/12 min-w-[15rem]">
+          <span className="text-gray-700">Password</span>
           <input
-            type="password" name="password"
-            class="
+            type="password" 
+            name="password" 
+            onChange={handleChange}
+            className="
                     mt-1
                     block
                     w-full
@@ -62,19 +62,23 @@ function LoginMain() {
                     shadow-sm
                     focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50
                   "
-          />
+                  required={true}/>
         </label>
         <Link to="/forgotpass" className="text-sm text-[#F74061]">
           Forgot password?
         </Link>
         <div className={LoaderCss.LoginBtnAndSpinner}>
-        <button onClick={handleClick} 
+          { !requesting &&
+            <>
+        <button
           type="submit"
           className="bg-[#F74061] rounded-lg w-36 p-2 text-white font-semibold hover:scale-110 transition-all ease-in-out"
         >
           Login
         </button>
-        <div className={variable?LoaderCss.preloaderFloatingCirclesDisappear:LoaderCss.preloaderFloatingCircles}>
+            </>
+          }
+        <div className={(!requesting)?LoaderCss.preloaderFloatingCirclesDisappear:LoaderCss.preloaderFloatingCircles}>
               <div className={LoaderCss.fCircleG} id={LoaderCss.frotateG_01}></div>
               <div className={LoaderCss.fCircleG} id={LoaderCss.frotateG_02}></div>
               <div className={LoaderCss.fCircleG} id={LoaderCss.frotateG_03}></div>
@@ -86,7 +90,7 @@ function LoginMain() {
             </div>
         </div>
         
-        <span class="text-sm text-[#F74061]">
+        <span className="text-sm text-[#F74061]">
           <span className="text-[#F79840]">Don't have an account?</span>
           <Link to="/login1"> Sign up</Link>
         </span>
