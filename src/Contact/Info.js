5254eb075327e87d5d74315fe4b5133b13e5ad8f
Samar Singh Randhawa <98534735+Samar1110@users.noreply.github.com>
2022-10-25 15:39:48 +0530

    Update Info.js

--- src/Contact/Info.js
+++ src/Contact/Info.js
@@ -25,10 +25,10 @@ export default function Info () {
             <div className='contactPageInfo'>
                 <h2 className='contactPageInfoHeading'>Publicity</h2>
                 <h3 className='contactPersonName'>Rudrarpit Patra</h3>
-                <a href='mailto:rudrarpit.patra.mec19@itbhu.ac.in' className='contactPersonEmail'>rudrarpit.patra.mec19@iitbhu.ac.in</a>
+                <a href='mailto:rudrarpit.patra.mec19@iitbhu.ac.in' className='contactPersonEmail'>rudrarpit.patra.mec19@iitbhu.ac.in</a>
                 <a href="tel:8763811512" className='contactCallInfo'>Call 8763811512</a>
                 <h3 className='contactPersonName'>Gaurav Jaiswal</h3>
-                <a href='mailto:gaurav.jaiswal.min19@itbhu.ac.in' className='contactPersonEmail'>gaurav.jaiswal.min19@iitbhu.ac.in</a>
+                <a href='mailto:gaurav.jaiswal.min19@iitbhu.ac.in' className='contactPersonEmail'>gaurav.jaiswal.min19@iitbhu.ac.in</a>
                 <a href="tel:9838660830" className='contactCallInfo'>Call 9838660830</a>
             </div>
             
