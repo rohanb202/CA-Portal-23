b20cc4a196ff57bba773b5b42e7e31cdf233cb62
Samar_Singh_Randhawa <samarsinghrandhawa11@gmail.com>
2022-10-28 23:05:25 +0530

    Added About Us and Event Registration

--- src/components/KY Main/main.jsx
+++ src/components/KY Main/main.jsx
@@ -1,4 +1,5 @@
 import Maincss from "./main.module.css"
+import Aboutcss from "./about.module.css"
 import { useState } from "react";
 import kylogo from "./kylogo.svg"
 import { Link } from "react-router-dom";
@@ -23,6 +24,7 @@ function Main(){
   }
 
     return (
+        <>
         <div className={Maincss.main}>
             <div className={Maincss.mobilenav}>
                 <i className="bg-gray-300 fa fa-bars" aria-hidden="true"></i>
@@ -36,7 +38,7 @@ function Main(){
                 </div>
                 <ul className={Maincss.nav}>
                     <li><Link to="/">TEAM</Link></li>
-                    <li><Link to="/">EVENTS</Link></li>
+                    <li><Link to="/eventRegistration">EVENTS</Link></li>
                     <li><Link to="/">FAQ</Link></li>
                 </ul>
             </div>
@@ -46,7 +48,7 @@ function Main(){
                     <div className={Maincss.navlist}>
                         <ul>
                             <li className={Maincss.navitem}><Link to="/">TEAM</Link></li>
-                            <li className={Maincss.navitem}><Link to="/">EVENTS</Link></li>
+                            <li className={Maincss.navitem}><Link to="/eventRegistration">EVENTS</Link></li>
                             <li className={Maincss.navitem}><Link to="/">FAQ</Link></li>
                         </ul>
                     </div>
@@ -95,6 +97,38 @@ function Main(){
                 </div>
             </div>
         </div>
+        <div className={Aboutcss.aboutSectionBody}>
+            <div className={Aboutcss.aboutTitle}>
+            <div className={Aboutcss.aboutBorder1}></div>
+            <div className={Aboutcss.aboutPageHeading} style={{color:"#F74061"}}>
+                About
+            </div>
+            <div className={Aboutcss.aboutPageHeading} style={{color:"#F74061"}}>
+                Us
+            </div>
+            <div className={Aboutcss.aboutBorder2}></div>
+            </div>
+            <div className={Aboutcss.aboutContent}>
+                <div className={Aboutcss.aboutContentText} style={{color:"white"}}>
+                Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.
+
+                It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+
+                It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+                </div>
+                <div className={Aboutcss.aboutContentImg1}></div>
+                <div className={Aboutcss.aboutContentImg2}></div>
+                <div className={Aboutcss.aboutContentImg3}></div>
+                <div className={Aboutcss.aboutContentMobileImg}>
+                <div className={Aboutcss.aboutContentMobileImg1}></div>
+                <div className={Aboutcss.aboutContentMobileImg2}></div>
+                <div className={Aboutcss.aboutContentMobileImg3}></div>
+                </div>
+            </div>
+            
+        </div>
+        </>
+        
     )
 }
 
