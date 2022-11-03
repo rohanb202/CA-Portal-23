2d0b3a0924d673c65af387653d9e99925fd6fa0e
MayankNagvanshi <87947863+MayankNagvanshi@users.noreply.github.com>
2022-11-03 18:45:36 +0530

    update payment&dash

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ import "./App.css";
 import LoginMain from "./components/LoginMain/LoginMain";
 import ForgotPass from "./components/ForgotPass/forgotPass";
 // import Main from './components/main/main';
+import Main from "./components/KY Main/main";
+import Payment from "./Pages/Payment/Payment";
 
 import "aos/dist/aos.css";
 
@@ -15,16 +17,16 @@ import { Routes, Route } from "react-router-dom";
 import LeaderBoard from "./components/leaderboard/LeaderBoard";
 import Profile from "./Pages/Profile/Profile";
 import AmbassadorApplyForm from "./Pages/Ambassodor/Application/ApplicationForm";
-import Payment from "./Pages/Payment/Payment";
 // import Login from './components/Login/Login';
-
+import EventRegistration from "./components/EventsRegistration/EventRegistration";
 // import { AuthProvider } from './context/AuthContext'
-import { ToastContainer } from "react-toastify";
+import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import RequireAuth from "./utils/RequireAuth";
 import GoogleMiddleware from "./Pages/GoogleLogin/GoogleMiddleware";
-import CompleteProfile from "./Pages/CompleteProfile/CompleteProfile";
+import CompleteProfile from "./Pages/CompleteProfile/CompleteProfile"
 import TestPage from "./Pages/TestPage";
+import Signup from "./components/Signup/signup";
 import Dashboard from "./Pages/Dashboard/Dashboard";
 
 function App() {
@@ -32,16 +34,7 @@ function App() {
     <>
       <ToastContainer />
       <Routes>
-        <Route path="/" element={<Home />}></Route>
-        <Route
-          path="/leaderboard"
-          element={
-            <RequireAuth>
-              <LeaderBoard />
-            </RequireAuth>
-          }
-        ></Route>
-        <Route path="/ambassador-apply" element={<AmbassadorApplyForm />} />
+        <Route path="/" element={<Main />}></Route>
         <Route
           path="/profile"
           element={
@@ -51,20 +44,25 @@ function App() {
           }
         />
         <Route path="/login" element={<LoginMain />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/eventRegistration" element={<EventRegistration />} />
         <Route path="/forgotpass" element={<ForgotPass />} />
-        <Route path="/payment" element={<Payment />} />
-        <Route
-          path="/profile"
-          element={
-            <RequireAuth>
-              <Profile />
-            </RequireAuth>
-          }
-        />
         <Route path="/api/google/callback" element={<GoogleMiddleware />} />
         {/* <Route path="/complete-profile" element={<CompleteProfile />} /> */}
         <Route path="/test" element={<TestPage />} />
+        {/* CA Specific Pages */}
+        <Route path="/ca" element={<Home />}></Route>
+        <Route path="/ca/ambassador-apply" element={<AmbassadorApplyForm />} />
+        <Route
+          path="/ca/leaderboard"
+          element={
+            <RequireAuth>
+              <LeaderBoard />
+            </RequireAuth>
+          }
+        ></Route>
         <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/payment" element={<Payment />} />
       </Routes>
     </>
   );
