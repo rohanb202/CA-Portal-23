f773dd4c99caa9162f2d996f5979c691d585343d
Samar Singh Randhawa <98534735+Samar1110@users.noreply.github.com>
2023-01-18 20:01:11 +0530

    Update Dashboard.js

--- src/Pages/Dashboard/Dashboard.js
+++ src/Pages/Dashboard/Dashboard.js
@@ -41,6 +41,8 @@ const style = {
   position: "absolute",
   top: "50%",
   left: "50%",
+  overflowY:"scroll",
+  maxHeight: '90vh',
   transform: "translate(-50%, -50%)",
   width: 400,
   bgcolor: "#F74061",
