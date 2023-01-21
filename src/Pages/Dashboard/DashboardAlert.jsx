05fea9e4ac0a089994588cbbbd6bac5196813e8b
Vysakh A V <vysakhav951@gmail.com>
2023-01-21 10:01:56 +0530

    updated events schedule

--- src/Pages/Dashboard/DashboardAlert.jsx
+++ src/Pages/Dashboard/DashboardAlert.jsx
@@ -28,7 +28,7 @@ export default function TransitionAlerts() {
           }
           sx={{ mb: 2 }}
         >
-          <a href="https://drive.google.com/file/d/1o4WJctcgPhI2q2dZyf_TPQVdIYJbi_6K/view?usp=sharing" target="_blank">CLICK HERE </a>to join the whatsapp group of your respective event! <a href="https://drive.google.com/file/d/1xb8z2ZU1hMCv1rjhio6JzvajDRtnWUmD/view?usp=sharing" target="_blank">CLICK HERE</a> for the events schedule!
+          <a href="https://drive.google.com/file/d/1o4WJctcgPhI2q2dZyf_TPQVdIYJbi_6K/view?usp=sharing" target="_blank">CLICK HERE to join the whatsapp group</a> of your respective event! <a href="https://drive.google.com/file/d/1gFcB0q4eN3XhwEzJrx1mmrW6YxvQTcnG/view?usp=sharing" target="_blank"><u>Please find the UPDATED SCHEDULE FOR KASHIYATRA '23 - Day 2 & 3 -  CLICK HERE!</u></a>
         </Alert>
       </Collapse>
     </Box>
