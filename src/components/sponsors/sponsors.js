981ec7d9cd940701126157ad4929c0f3b452a9a7
Rew0611 <harshit.swarnkar.ece21@itbhu.ac.in>
2023-03-05 22:46:38 +0530

    changes

--- src/components/sponsors/sponsors.js
+++ src/components/sponsors/sponsors.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-unreachable */
 import React, { useState, useEffect, useContext } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import AuthContext from "../../context/AuthContext";
