04d651c642a4c515ffff1233be1bd5afdde16394
Vysakh <57103973+Yshaq@users.noreply.github.com>
2023-07-27 01:34:31 +0530

    bring back team page

--- src/App.js
+++ src/App.js
@@ -67,7 +67,7 @@ function App() {
 
         <Route path="/events" element={<Events/>} />
 
-        {/*<Route path="/team" element={<Team />} />*/}
+        <Route path="/team" element={<Team />} />
         <Route path="/sponsors" element={<Sponsors />} />
         <Route path="/preevents" element={<PreEvents />} />
         <Route path="/forgotpass" element={<ForgotPass />} />
